dc06402aa33c1e1ed9ca74ea9ad27a6bdbc97733
Sahan Seneviratne <sasiniseneviratne@gmail.com>
2023-05-12 11:57:05 +0100

    finished matchers

--- src/test/java/com/mockitotutorial/happyhotel/booking/Test06Matchers.java
+++ src/test/java/com/mockitotutorial/happyhotel/booking/Test06Matchers.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.function.Executable;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.*;
 
 public class Test06Matchers {
 
@@ -29,23 +29,32 @@ public class Test06Matchers {
         this.bookingService = new BookingService(paymentServiceMock, roomServiceMock,
                 bookingDAOMock, mailSenderMock);
     }
-    
-    @Test
-    @DisplayName("should throw exception when room is not available")
-    public void shouldThrowExceptionRoomIsNotAvailable() {
-        //given
-        BookingRequest bookingRequestFail = new BookingRequest("1", LocalDate.of(2020,01,01),
-                LocalDate.of(2020,01,005),2,false);
 
-        when(this.roomServiceMock.findAvailableRoomId(bookingRequestFail))
-                .thenThrow(BusinessException.class);
+
+    @Test
+    @DisplayName("Should not complete booking when price too high")
+    public void shouldNotCompleteBookingWhenPriceTooHigh() {
+        //given
+        BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020,01,01),
+                LocalDate.of(2020,01,005),2,true);
+
+        //any() makes it so that any version of the object in the brackets is used, anyDouble() is for primitive types
+        //instead of anyDouble, for specific values use eq() to make equals
+        when(this.paymentServiceMock.pay(any(), eq(400.0))).thenThrow(BusinessException.class);
 
         //when
-        Executable executable = () -> bookingService.makeBooking(bookingRequestFail);
+        Executable executable = () -> bookingService.makeBooking(bookingRequest);
 
         //then
         assertThrows(BusinessException.class,executable);
-    }
+        }
+/*
+Matches rules,
 
+Use any() for objects, for primitives use anyDouble(), anyBoolean() etc
 
+use eq() to mix matchers and concrete values:  mathos(any(), eq(400.0))
+
+for nullable strings, use any(), anyString() will not use null value
+ */
 }
